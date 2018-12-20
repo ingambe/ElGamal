62da296784ce2a9016b7475a80f936fcd8bc3ebc
Pierre TASSEL <pierre.tassel@outlook.fr>
2018-12-20 22:44:37 +0100

    Add coprime generator

--- Main.py
+++ Main.py
@@ -1,15 +1,18 @@
 from random import randint
 from math import sqrt
 
+# return an odd number between 2**16 and 2**31
 def generateBigOddNumber():
     return (2 * randint(2**16, 2**31)) + 1
 
+# return a big prime number (2**16 < x < 2**32)
 def generateBigPrimeNumber():
     big_odd_number = generateBigOddNumber()
     while not(is_prime(big_odd_number)):
         big_odd_number = generateBigOddNumber()
     return big_odd_number
 
+# test the primality of a number
 def is_prime(x):
     if x % 2 == 0:
         return False
@@ -20,4 +23,14 @@ def is_prime(x):
             return False
     return True
 
-print(generateBigPrimeNumber())
+def generateASmallerPrimeNumber(x):
+    # generate a number between 2 and x-1
+    startNumber = randint(2, x)
+    # if we haven't generate 2 and startNumber is even, we change startNumber to an odd number
+    if startNumber != 2 and startNumber % 2 == 0:
+        startNumber -= 1
+    while not(is_prime(startNumber)) and startNumber > 2:
+        startNumber -= 2
+    return startNumber
+
+print(generateASmallerPrimeNumber(generateBigPrimeNumber()))
